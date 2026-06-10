f1d0c9522466516cfd6625d93a0dde2cc2a72f19
Rakesh Kuratti <83536408+Rakesh-vk@users.noreply.github.com>
2026-06-10 19:35:42 +0530

    Create SortingAlgos.java
    Adding bubble sort

---
+++ src/SortingAlgos.java
@@ -0,0 +1,23 @@
+public class Main {
+
+    public static void main(String[] args) {
+        int[] arr={5,1,4,3};
+        bubbleSort(arr);
+    }
+    public static void bubbleSort(int[] arr){
+        int n= arr.length;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n-i-1;j++){
+                if(arr[j]>arr[j+1]){
+                    int temp = arr[j];
+                    arr[j]=arr[j+1];
+                    arr[j+1]= temp;
+                }
+            }
+        }
+        for(int i:arr){
+            System.out.print(i+" ");
+        }
+    }
+
+}
