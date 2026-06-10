3b5e4bd1c758f3d63708a386451736341b4ebd17
Rakesh Kuratti <83536408+Rakesh-vk@users.noreply.github.com>
2026-06-10 19:52:06 +0530

    Update SortingAlgos.java
    Adding InsertionSort

--- src/SortingAlgos.java
+++ src/SortingAlgos.java
@@ -38,5 +38,23 @@ public class Main {
             System.out.print(i+" ");
         }
     }
+	public static void InsertionSort(int[] arr) {
 
+    for (int i = 1; i < arr.length; i++) {
+
+        int current = arr[i];
+        int j = i - 1;
+
+        while (j >= 0 && arr[j] > current) {
+            arr[j + 1] = arr[j];
+            j--;
+        }
+
+        arr[j + 1] = current;
+    }
+
+    for (int num : arr) {
+        System.out.print(num + " ");
+    }
+}
 }
