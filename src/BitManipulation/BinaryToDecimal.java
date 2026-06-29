e3fac97b1d95c0e444dfd9265a17dbf0d1d517fe
Rakesh Kuratti <83536408+Rakesh-vk@users.noreply.github.com>
2026-06-29 11:51:24 +0530

    bitwise manipulation

---
+++ src/BitManipulation/BinaryToDecimal.java
@@ -0,0 +1,16 @@
+package BitManipulation;
+
+public class BinaryToDecimal {
+    public static void main(String[] args) {
+        String binary = "0101";
+        int j=0,num=0;
+        for(int i=binary.length()-1;i>=0;i--){
+            char ch = binary.charAt(i);
+            if(ch=='1'){
+                num += Math.pow(2,j);
+            }
+            j++;
+        }
+        System.out.println(num);
+    }
+}
---
+++ src/BitManipulation/DecimalToBinary.java
@@ -0,0 +1,11 @@
+package BitManipulation;
+
+public class DecimalToBinary {
+    public static void main(String[] args) {
+        int num =2;
+       int j=0;
+       while(num > 0){
+           System.out.println(num);
+       }
+    }
+}
