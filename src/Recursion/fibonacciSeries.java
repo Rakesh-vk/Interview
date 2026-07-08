5e48c8d93fde1dd701bb5444426852d3296a8516
Rakesh Kuratti <83536408+Rakesh-vk@users.noreply.github.com>
2026-07-08 20:18:59 +0530

    recursion basics

---
+++ src/Recursion/fibonacciSeries.java
@@ -0,0 +1,13 @@
+package Recursion;
+
+public class fibonacciSeries {
+    public static void main(String[] args) {
+        System.out.println(fibo(10));
+    }
+    public static int fibo(int n){
+        if(n==1 || n==0){
+            return 1;
+        }
+        return fibo(n-1) + fibo(n-2);
+    }
+}
