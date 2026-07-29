75e34e7dac6ccf50a471d1c4fdfe545dab66b18f
Rakesh Kuratti <83536408+Rakesh-vk@users.noreply.github.com>
2026-07-29 22:09:41 +0530

    merge Intervals

---
+++ src/leetcode/Prefix/_238ProductOfArrayExceptSelf.java
@@ -0,0 +1,8 @@
+package leetcode.Prefix;
+
+public class _238ProductOfArrayExceptSelf {
+    public static void main(String[] args) {
+
+    }
+
+}
---
+++ src/leetcode/_56MergeIntervals.java
@@ -0,0 +1,18 @@
+package leetcode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class _56MergeIntervals {
+    public static void main(String[] args) {
+        int[][] intervals={{1,3},{2,6},{8,10},{15,18}};
+
+
+    }
+    public static int[][] merge(int[][] intervals) {
+        int n= intervals.length;
+        List<List<Integer>> merged= new ArrayList<>();
+        
+
+    }
+}
