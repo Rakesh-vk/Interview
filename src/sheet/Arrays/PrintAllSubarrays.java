755b0356f98662e0b513ff02d228a05244e3eae5
Rakesh Kuratti <83536408+Rakesh-vk@users.noreply.github.com>
2026-08-05 18:59:32 +0530

    PrintAllSubarrays

---
+++ src/sheet/Arrays/PrintAllSubarrays.java
@@ -0,0 +1,21 @@
+package sheet.Arrays;
+
+
+public class PrintAllSubarrays {
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4, 5};
+        solution(arr);
+    }
+
+    public static void solution(int[] arr) {
+        int n=arr.length;
+        for(int i=0;i<n;i++){
+            for(int j=i;j<n;j++){
+                for (int k=i;k<=j;k++){
+                    System.out.print(arr[k]+" ");
+                }
+                System.out.println();
+            }
+        }
+    }
+}
